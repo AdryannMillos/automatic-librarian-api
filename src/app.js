f74f299f18498ad3b19538089c4c9e3640fa59e4
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2022-12-17 12:47:34 -0300

    wip: add cors lib

--- src/app.js
+++ src/app.js
@@ -1,35 +1,16 @@
 const bodyParser = require("body-parser");
 const schedule = require("node-schedule");
 const express = require("express");
+const cors = require('cors');
+
 const CompareEventUrlService = require("../src/services/CompareEventUrlService");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // // Request headers you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", false);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 
 app.use("/api/v1/leviathan", require("./routes/api.leviathan.route"));
 
