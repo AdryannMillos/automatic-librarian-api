7241609f3b172a8fa4edacce8e06ddaa5ffed23a
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2022-12-17 16:13:12 -0300

    wip: fix cors middleware

--- src/app.js
+++ src/app.js
@@ -9,8 +9,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
-app.options('*', cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use("/api/v1/leviathan", require("./routes/api.leviathan.route"));
 
