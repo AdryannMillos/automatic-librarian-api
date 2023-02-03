a649ff71f0fe2248724f588123752c36a6460a7b
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-02-03 14:04:12 -0300

    fea: add db port

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
     },
     test: {
         username: process.env.DB_USER,
@@ -14,6 +15,7 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
     },
     production: {
         username: process.env.DB_USER,
@@ -21,5 +23,6 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
     },
 };
